97970ef8d0435b1ac9c409b078a6d250d44d849d
orynes <a.mochanry2468@gmail.com>
2024-04-07 04:04:49 +0300

    add file

--- index.js
+++ index.js
@@ -1,84 +1,91 @@
+
 module.exports = function PartyDeathMarkers(mod) {
-  let members = []
-  let markers = []
+	let members = [];
+	let markers = [];
 
-  mod.game.on('enter_game', removeAllMarkers)
+	mod.game.on("enter_game", removeAllMarkers);
 
-  mod.hook('S_PARTY_MEMBER_LIST', 7, event => {
-    members = event.members
-  })
+	mod.hook("S_PARTY_MEMBER_LIST", "*", event => {
+		members = event.members;
+	});
 
-  mod.hook('S_DEAD_LOCATION', 2, event => {
-    spawnMarker(members.find(member => member.gameId === event.gameId), event.loc)
-  })
+	mod.hook("S_DEAD_LOCATION", "*", event => {
+		spawnMarker(members.find(member => member.gameId === event.gameId), event.loc);
+	});
 
-  mod.hook('S_SPAWN_USER', 15, event => {
-    if (!event.alive)
-      spawnMarker(members.find(member => member.gameId === event.gameId), event.loc)
-  })
+	mod.hook("S_SPAWN_USER", "*", event => {
+		if (!event.alive)
+			spawnMarker(members.find(member => member.gameId === event.gameId), event.loc);
+	});
 
-  mod.hook('S_PARTY_MEMBER_STAT_UPDATE', 3, event => {
-    if (mod.game.me.playerId === event.playerId)
-      return
+	mod.hook("S_PARTY_MEMBER_STAT_UPDATE", "*", event => {
+		if (mod.game.me.playerId === event.playerId)
+			return;
 
-    if (markers.length > 0 && event.alive && event.curHp > 0)
-      removeMarker(members.find(member => member.playerId === event.playerId))
-  })
+		if (markers.length > 0 && event.alive && event.curHp > 0)
+			removeMarker(members.find(member => member.playerId === event.playerId));
+	});
 
-  mod.hook('S_LEAVE_PARTY_MEMBER', 2, event => {
-    removeMarker(members.find(member => member.playerId === event.playerId))
-  })
+	mod.hook("S_EACH_SKILL_RESULT", "*", event => {
+		if (event.skill.id === 60410102 || event.skill.id === 120100) {
+			removeMarker(members.find(member => member.gameId === event.target));
+		}
+	});
 
-  mod.hook('S_LEAVE_PARTY', 1, () => {
-    removeAllMarkers()
-    members = []
-  })
+	mod.hook("S_LEAVE_PARTY_MEMBER", "*", event => {
+		removeMarker(members.find(member => member.playerId === event.playerId));
+	});
 
-  function spawnMarker(member, loc) {
-    if (!member || mod.game.me.is(member.gameId))
-      return
+	mod.hook("S_LEAVE_PARTY", "*", () => {
+		removeAllMarkers();
+		members = [];
+	});
 
-    removeMarker(member)
-    markers.push(member.playerId)
+	function spawnMarker(member, loc) {
+		if (!member || mod.game.me.is(member.gameId))
+			return;
 
-    mod.toClient('S_SPAWN_DROPITEM', 8, {
-      gameId: member.playerId,
-      loc: loc,
-      item: getMarker(member.class),
-      amount: 1,
-      expiry: 999999,
-      owners: [{playerId: mod.game.me.playerId}]
-    })
-  }
+		removeMarker(member);
+		markers.push(member.playerId);
 
-  function getMarker(classid) {
-    switch (classid) {
-      case 1:
-      case 10:
-        return 91177
-      case 6:
-      case 7:
-        return 91113
-      default:
-        return 98260
-    }
-  }
+		mod.toClient("S_SPAWN_DROPITEM", "*", {
+			gameId: member.playerId,
+			loc: loc.addN(0, 0, -100),
+			item: getMarker(member.class),
+			amount: 1,
+			expiry: 0,
+			owners: [{ playerId: mod.game.me.playerId }]
+		});
+	}
 
-  function removeMarker(member) {
-    if (!member)
-      return
+	function getMarker(classid) {
+		switch (classid) {
+			case 1:
+			case 10:
+				return 46703;
+			case 6:
+			case 7:
+				return 89141;
+			default:
+				return 89604;
+		}
+	}
 
-    const id = member.playerId
-    if (markers.includes(id)) {
-      mod.toClient('S_DESPAWN_DROPITEM', 4, {
-        gameId: id
-      })
-      markers = markers.filter(marker => marker !== id)
-    }
-  }
+	function removeMarker(member) {
+		if (!member)
+			return;
 
-  function removeAllMarkers() {
-    members.forEach(member => removeMarker(member))
-    markers = []
-  }
-}
+		const id = member.playerId;
+		if (markers.includes(id)) {
+			mod.toClient("S_DESPAWN_DROPITEM", "*", {
+				gameId: id
+			});
+			markers = markers.filter(marker => marker !== id);
+		}
+	}
+
+	function removeAllMarkers() {
+		members.forEach(member => removeMarker(member));
+		markers = [];
+	}
+};
